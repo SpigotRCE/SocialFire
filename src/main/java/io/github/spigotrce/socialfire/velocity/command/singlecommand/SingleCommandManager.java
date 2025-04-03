73ea2e52be70580573e4334d604fa636c4e89ee0
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-04-03 15:45:34 +0530

    feat: send announcements command

--- src/main/java/io/github/spigotrce/socialfire/velocity/command/singlecommand/SingleCommandManager.java
+++ src/main/java/io/github/spigotrce/socialfire/velocity/command/singlecommand/SingleCommandManager.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandSource;
 import io.github.spigotrce.socialfire.velocity.command.AbstractBrigadierCommand;
+import io.github.spigotrce.socialfire.velocity.command.singlecommand.impl.AnnouncementCommand;
 import io.github.spigotrce.socialfire.velocity.command.singlecommand.impl.ReloadCommand;
 
 public class SingleCommandManager extends AbstractBrigadierCommand {
@@ -14,6 +15,7 @@ public class SingleCommandManager extends AbstractBrigadierCommand {
         command = LiteralArgumentBuilder.literal(super.commandName);
         command.requires(invoker -> invoker.hasPermission("socialfire.admin"));
         registerCommand(new ReloadCommand());
+        registerCommand(new AnnouncementCommand());
         super.register();
     }
 
---
+++ src/main/java/io/github/spigotrce/socialfire/velocity/command/singlecommand/impl/AnnouncementCommand.java
@@ -0,0 +1,61 @@
+package io.github.spigotrce.socialfire.velocity.command.singlecommand.impl;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.velocitypowered.api.command.BrigadierCommand;
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.event.command.CommandExecuteEvent;
+import com.velocitypowered.api.proxy.Player;
+import io.github.spigotrce.socialfire.velocity.VelocityFire;
+import io.github.spigotrce.socialfire.velocity.command.AbstractBrigadierCommand;
+import net.kyori.adventure.text.Component;
+
+import java.io.IOException;
+
+public class AnnouncementCommand extends AbstractBrigadierCommand {
+    public AnnouncementCommand() {
+        super(false, "announcement");
+    }
+
+    @Override
+    public BrigadierCommand build() {
+        return new BrigadierCommand(LiteralArgumentBuilder.<CommandSource>literal(super.commandName)
+                .executes(commandContext -> {
+                    commandContext.getSource().sendMessage(Component.text("Incomplete command!"));
+                    return SINGLE_SUCCESS;
+                }).then(RequiredArgumentBuilder.<CommandSource, String>argument("name", StringArgumentType.word())
+                        .suggests((ctx, builder) -> {
+                            String partialType;
+
+                            try {
+                                partialType = ctx.getArgument("name", String.class).toLowerCase();
+                            } catch (IllegalArgumentException ignored) {
+                                partialType = "";
+                            }
+
+                            if (partialType.isEmpty()) {
+                                VelocityFire.CONFIG.getLinks().keySet().forEach(builder::suggest);
+                                return builder.buildFuture();
+                            }
+
+                            String finalPartialType = partialType;
+
+                            VelocityFire.CONFIG.getLinks().keySet().stream().filter(name -> name.toLowerCase().startsWith(finalPartialType)).forEach(builder::suggest);
+
+                            return builder.buildFuture();
+                        })
+
+                        .executes(commandContext -> {
+                            String name = commandContext.getArgument("name", String.class);
+
+                            for (Player player : VelocityFire.PROXY_SERVER.getAllPlayers()) {
+                                VelocityFire.ANNOUNCEMENT_MANAGER.sendAnnouncement(player, VelocityFire.CONFIG.getLinks().get(name));
+                            }
+
+                            return SINGLE_SUCCESS;
+                        })
+                )
+        );
+    }
+}
